acf314d9d1f7ea1f305339bbfc2cb4f0dfe20d65
Stefan <Stefan@DESKTOP-1HKT4DK>
2022-11-21 16:57:18 +0100

    Add project files.

---
+++ Kolokvijum1/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Kolokvijum1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fontBoldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontItalicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectFontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDlg = new System.Windows.Forms.SaveFileDialog();
+            this.fontDlg = new System.Windows.Forms.FontDialog();
+            this.colorDlg = new System.Windows.Forms.ColorDialog();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.lblReplace = new System.Windows.Forms.Label();
+            this.btnSearchReplace = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblIme = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(242)))), ((int)(((byte)(162)))));
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpen.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnOpen.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnOpen.Image = ((System.Drawing.Image)(resources.GetObject("btnOpen.Image")));
+            this.btnOpen.Location = new System.Drawing.Point(14, 13);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(114, 55);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // openFileDlg
+            // 
+            this.openFileDlg.FileName = "openFileDialog1";
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(74)))), ((int)(((byte)(74)))));
+            this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox.ContextMenuStrip = this.contextMenuStrip1;
+            this.richTextBox.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox.Location = new System.Drawing.Point(14, 83);
+            this.richTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(906, 464);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontBoldToolStripMenuItem,
+            this.fontItalicToolStripMenuItem,
+            this.fontColorToolStripMenuItem,
+            this.selectFontToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(133, 92);
+            // 
+            // fontBoldToolStripMenuItem
+            // 
+            this.fontBoldToolStripMenuItem.Name = "fontBoldToolStripMenuItem";
+            this.fontBoldToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.fontBoldToolStripMenuItem.Text = "Font Bold";
+            this.fontBoldToolStripMenuItem.Click += new System.EventHandler(this.fontBoldToolStripMenuItem_Click);
+            // 
+            // fontItalicToolStripMenuItem
+            // 
+            this.fontItalicToolStripMenuItem.Name = "fontItalicToolStripMenuItem";
+            this.fontItalicToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.fontItalicToolStripMenuItem.Text = "Font Italic";
+            this.fontItalicToolStripMenuItem.Click += new System.EventHandler(this.fontItalicToolStripMenuItem_Click);
+            // 
+            // fontColorToolStripMenuItem
+            // 
+            this.fontColorToolStripMenuItem.Name = "fontColorToolStripMenuItem";
+            this.fontColorToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.fontColorToolStripMenuItem.Text = "Font Color";
+            this.fontColorToolStripMenuItem.Click += new System.EventHandler(this.fontColorToolStripMenuItem_Click);
+            // 
+            // selectFontToolStripMenuItem
+            // 
+            this.selectFontToolStripMenuItem.Name = "selectFontToolStripMenuItem";
+            this.selectFontToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.selectFontToolStripMenuItem.Text = "Select Font";
+            this.selectFontToolStripMenuItem.Click += new System.EventHandler(this.selectFontToolStripMenuItem_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(242)))), ((int)(((byte)(162)))));
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.Location = new System.Drawing.Point(670, 14);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(117, 53);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(74)))), ((int)(((byte)(74)))));
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearch.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtSearch.Location = new System.Drawing.Point(224, 14);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(116, 20);
+            this.txtSearch.TabIndex = 3;
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(74)))), ((int)(((byte)(74)))));
+            this.txtReplace.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtReplace.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtReplace.Location = new System.Drawing.Point(224, 44);
+            this.txtReplace.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(116, 20);
+            this.txtReplace.TabIndex = 4;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSearch.Location = new System.Drawing.Point(158, 15);
+            this.lblSearch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(62, 19);
+            this.lblSearch.TabIndex = 5;
+            this.lblSearch.Text = "Search";
+            // 
+            // lblReplace
+            // 
+            this.lblReplace.AutoSize = true;
+            this.lblReplace.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblReplace.Location = new System.Drawing.Point(151, 45);
+            this.lblReplace.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblReplace.Name = "lblReplace";
+            this.lblReplace.Size = new System.Drawing.Size(68, 19);
+            this.lblReplace.TabIndex = 6;
+            this.lblReplace.Text = "Replace";
+            // 
+            // btnSearchReplace
+            // 
+            this.btnSearchReplace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(242)))), ((int)(((byte)(162)))));
+            this.btnSearchReplace.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearchReplace.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearchReplace.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchReplace.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.btnSearchReplace.Image = ((System.Drawing.Image)(resources.GetObject("btnSearchReplace.Image")));
+            this.btnSearchReplace.Location = new System.Drawing.Point(348, 13);
+            this.btnSearchReplace.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnSearchReplace.Name = "btnSearchReplace";
+            this.btnSearchReplace.Size = new System.Drawing.Size(107, 53);
+            this.btnSearchReplace.TabIndex = 7;
+            this.btnSearchReplace.UseVisualStyleBackColor = false;
+            this.btnSearchReplace.Click += new System.EventHandler(this.btnSearchReplace_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(242)))), ((int)(((byte)(162)))));
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(793, 13);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(126, 55);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblIme
+            // 
+            this.lblIme.AutoSize = true;
+            this.lblIme.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblIme.Location = new System.Drawing.Point(489, 30);
+            this.lblIme.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblIme.Name = "lblIme";
+            this.lblIme.Size = new System.Drawing.Size(146, 19);
+            this.lblIme.TabIndex = 9;
+            this.lblIme.Text = "Stefan Bogdanovic";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.lblIme);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSearchReplace);
+            this.Controls.Add(this.lblReplace);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.btnOpen);
+            this.Font = new System.Drawing.Font("Bookman Old Style", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.SystemColors.Info;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MinimumSize = new System.Drawing.Size(950, 600);
+            this.Name = "Form1";
+            this.Text = "Stefan Bogdanovic s13/20";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDlg;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDlg;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fontBoldToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontItalicToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectFontToolStripMenuItem;
+        private System.Windows.Forms.FontDialog fontDlg;
+        private System.Windows.Forms.ColorDialog colorDlg;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.Label lblReplace;
+        private System.Windows.Forms.Button btnSearchReplace;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblIme;
+    }
+}
+
---
+++ Kolokvijum1/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Kolokvijum1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            openFileDlg.Title = "Otvorite fajl:";
+
+
+            //ovde postavljamo filter koji ce se tip fajla otvoriti
+            openFileDlg.Filter = "Rich Text Format|*.rtf|Text file|*.txt";
+
+            //proveravamo greske
+            try
+            {
+                //ukoliko je fajl otvoren
+                if (openFileDlg.ShowDialog() == DialogResult.OK)
+                {
+                    //ukoliko fajl ima ime
+                    if (openFileDlg.FileName != "")
+                    {
+                        //u if-uu trazimo ekstenziju rtf, i ukoliko to jeste ekstenzija, ispisujemo tekst sa detaljima
+                        if (Path.GetExtension(openFileDlg.FileName) == ".rtf")
+                        {
+                            richTextBox.LoadFile(openFileDlg.FileName, RichTextBoxStreamType.RichText);
+                        }
+                        //ukoliko ekstenzija nije rtf ispisujemo tekst bez ikakvih detalja
+                        else
+                        {
+                            richTextBox.LoadFile(openFileDlg.FileName, RichTextBoxStreamType.PlainText);
+                        }
+                    }
+                }
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.ToString());
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveFileDlg.Title = "Gde zelite da sacuvate fajl?";
+
+            //zadaje da mozemo da sacuvamo fajl samo kao rtf fajl
+
+            saveFileDlg.Filter = " Rich Text Format|*.rtf ";
+
+            //cuvanje u fajl
+            try {
+                if (saveFileDlg.ShowDialog() == DialogResult.OK)
+                {
+                    //kao argument uzima putanju gde ce se fajl sacuvati
+                    richTextBox.SaveFile(saveFileDlg.FileName);
+
+
+                }
+            }
+            catch(Exception greska)
+            {
+                MessageBox.Show(greska.ToString()); ;
+            }
+        }
+
+
+        //odabir fonta
+        private void selectFontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (fontDlg.ShowDialog() == DialogResult.OK)
+                {
+                    richTextBox.SelectionFont = fontDlg.Font;
+
+                }
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.ToString()); ;
+            }
+        }
+
+        //bold
+        private void fontBoldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Bold);
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.ToString()); ;
+            }
+        }
+
+        //italic
+        private void fontItalicToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, FontStyle.Italic);
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.ToString()); ;
+            }
+        }
+
+
+        //odabir boje
+        private void fontColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                if (colorDlg.ShowDialog() == DialogResult.OK)
+                {
+                    richTextBox.SelectionColor = colorDlg.Color;
+                }
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.ToString()); ;
+            }
+        }
+
+        private void btnSearchReplace_Click(object sender, EventArgs e)
+        {
+            //pretvaramo vrednost iz textboxova u string kako bi se njima moglo manipulisati
+
+            //textbox koji trazimo
+            string search = txtSearch.Text.Trim();
+
+            //textbox kojim menjamo
+            string replace = txtReplace.Text.Trim();
+
+            //richtextbox u kome se sav text nalazi
+            string text = richTextBox.Text.Trim();
+
+            //tekst koji ce kasnije biti prikazan
+            string replaced;
+
+            try
+            {
+                //ako je richtextbox prazan
+                if (text == "")
+                {
+                    MessageBox.Show("Richtext mora imati neki sadrzaj");
+                    richTextBox.Focus();
+                }
+                //ako je search prazan
+                else if (search == "")
+                {
+                    MessageBox.Show("search prazan");
+                    txtSearch.Focus();
+                    
+                }
+                //ako je replace prazan
+                else if (replace == "")
+                {
+                    MessageBox.Show("replace prazan");
+                    txtReplace.Focus();
+
+                }
+                //svi uslovi su ispunjeni
+                else
+                {
+                    replaced = text.Replace(search, replace);
+                    richTextBox.Text = replaced;
+
+                }
+
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.ToString());
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //ilazak iz aplikacije
+            try
+            {
+                Application.Exit();
+            }
+            catch (Exception greska){
+                MessageBox.Show(greska.ToString());
+            }
+        }
+    }
+}
